df86a88fc754252d959f8eb7dd2d82f439aa6265
Sean <jb@JBs-MacBook-Pro.local>
2021-04-29 18:56:29 -0500

    live student cards added

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SharkTank from '../components/SharkTank';
+import { dearlyBeloved, followTheLight, livingStudents } from '../helpers/data/studentData';
 import './App.scss';
 
 function App() {
+  const [liveStudents, setLiveStudents] = useState([]);
+  const [deadStudents, setDeadStudents] = useState([]);
+
+  useEffect(() => {
+    setLiveStudents(livingStudents());
+    setDeadStudents(dearlyBeloved());
+    followTheLight();
+  }, []);
+
+  console.warn(liveStudents);
+  console.warn(deadStudents);
+
   return (
-    <SharkTank />
+    <SharkTank liveStudents={liveStudents} setLiveStudents={setLiveStudents} />
   );
 }
 
--- src/components/SharkTank.js
+++ src/components/SharkTank.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Grid } from '@material-ui/core';
 import Header from './Header';
+import LiveStudents from './LiveStudent';
 
-const SharkTank = () => (
+const SharkTank = ({ liveStudents, setLiveStudents }) => (
     <Grid container direction='column'>
       <Grid item>
         <Header />
@@ -10,10 +12,23 @@ const SharkTank = () => (
       <Grid item container>
         <Grid item xs={2} />
           <Grid item xs={8}>
+          {liveStudents.map((student) => (
+            <LiveStudents
+               key={student.id}
+               firstName={student.firstName}
+               lastName={student.lastName}
+               setLiveStudents={setLiveStudents}
+            />
+          ))}
           </Grid>
         <Grid item xs={2} />
       </Grid>
     </Grid>
 );
+
+SharkTank.propTypes = {
+  liveStudents: PropTypes.array.isRequired,
+  setLiveStudents: PropTypes.func
+};
 
 export default SharkTank;
